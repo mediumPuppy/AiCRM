02c27f3c3a29fd43b4a84b5ab3bed6456e790f23
jefferson gauntlet <mediumPuppy@users.noreply.github.com>
2025-01-27 20:15:12 -0700

    more jest issues

---
+++ client/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1'
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest'
+  }
+} 
